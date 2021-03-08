69872f7062e3bbd87dfd4895560c7addd519b4d5
Abdulhakim Fışkın <abdulhakimfiskin@gmail.com>
2021-03-08 17:29:52 +0300

    27 Şubat Ders Sonu

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,8 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -14,5 +16,12 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir.";
         public static string ProductNameAlreadyExists = "Bu isimde zaten başka bir ürün mevcut.";
         public static string CategoryLimitExceeded = "Kategori limiti aşıldığı için yeni ürün eklenemez.";
+        public static string AuthorizationDenied = "Yetkiniz yok.";
+        public static string SuccessfulLogin = "Giriş başarılı.";
+        public static string UserAlreadyExists = "Kullanıcı mevcut.";
+        public static string AccessTokenCreated = "Token oluşturuldu.";
+        public static string UserRegistered = "Kayıt tamamlandı.";
+        public static string UserNotFound = "Kullanıcı bulunamadı.";
+        public static string PasswordError = "Parola hatalı.";
     }
 }
